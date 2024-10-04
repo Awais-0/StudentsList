5ece47904b265f64be1c0f00c575eb45b4b76b4f
M. Awais Raza <ar1428391@gmail.com>
2024-10-04 20:32:10 +0500

    first commit

---
+++ students-fe/src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_URL = "http://localhost:8000/api/students/";
+// export const API_URL_SHARP = "http://localhost:5194/WeatherForecast";
---
+++ students-fe/src/constants/index2.js
@@ -0,0 +1,1 @@
+export const API_URL_SHARP = "http://localhost:5194/WeatherForecast";
